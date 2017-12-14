f06d523321887ef345d28a23a67fe9d3c6457c5b
Brittany Martin <brittany.jill.martin@gmail.com>
2017-12-14 16:25:19 -0500

    Add in solutions for depth first search. elimination, only choice and reducing the puzzle

--- solution.py
+++ solution.py
@@ -1,7 +1,6 @@
 
 from utils import *
 
-
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
@@ -61,8 +60,12 @@ def eliminate(values):
     dict
         The values dictionary with the assigned values eliminated from peers
     """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    for box in solved_values:
+        digit = values[box]
+        for peer in peers[box]:
+            values[peer] = values[peer].replace(digit,'')
+    return values
 
 
 def only_choice(values):
@@ -85,8 +88,12 @@ def only_choice(values):
     -----
     You should be able to complete this function by copying your code from the classroom
     """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    for unit in unitlist:
+        for digit in '123456789':
+            dplaces = [box for box in unit if digit in values[box]]
+            if len(dplaces) == 1:
+                values[dplaces[0]] = digit
+    return values
 
 
 def reduce_puzzle(values):
@@ -101,10 +108,19 @@ def reduce_puzzle(values):
     -------
     dict or False
         The values dictionary after continued application of the constraint strategies
-        no longer produces any changes, or False if the puzzle is unsolvable 
+        no longer produces any changes, or False if the puzzle is unsolvable
     """
-    # TODO: Copy your code from the classroom and modify it to complete this function
-    raise NotImplementedError
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    stalled = False
+    while not stalled:
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        values = eliminate(values)
+        values = only_choice(values)
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        stalled = solved_values_before == solved_values_after
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
 
 
 def search(values):
@@ -126,8 +142,38 @@ def search(values):
     You should be able to complete this function by copying your code from the classroom
     and extending it to call the naked twins strategy.
     """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    # First, reduce the puzzle using the previous function
+    values = reduce_puzzle(values)
+
+    # check to make sure the reduced puzzle is not solved/errored out
+    if values is False:
+        # errored?
+        return False
+    # The all() method returns True when all elements in the given iterable are true. If not, it returns False.
+    # Does each value for the boxes dictionary have a length of 1?
+    if all(len(values[box]) == 1 for box in boxes):
+        # If sudoku is solved
+        return values
+    # Choose one of the unfilled squares with the fewest possibilities
+    # we are only concerned with the values contained in the box
+    _, box = min((len(values[box]), box) for box in values if len(values[box]) > 1)
+    # Now use recursion to solve each one of the resulting sudokus, and
+    # if one returns a value (not False), return that answer!
+
+    # iterate over each number in the box. Example 5 & 6 from '56'.
+    for digit in values[box]:
+        # create a shallow copy of the puzzle to test, our puzzle in the UpsideDown
+        new_values = values.copy()
+        # set the box in question to the experimental digit
+        new_values[box] = digit
+        # call the function (itself) to see if the Sudoku with this
+        # missing test number will solve the puzzle.
+        # Remember: when a thing is defined in terms of itself or of its type, it is recursive
+        is_solved = search(new_values)
+        # if it returns anything other than false, we are solved!
+        if is_solved:
+            # return the solved puzzle
+            return is_solved
 
 
 def solve(grid):
@@ -137,7 +183,7 @@ def solve(grid):
     ----------
     grid(string)
         a string representing a sudoku grid.
-        
+
         Ex. '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
 
     Returns
